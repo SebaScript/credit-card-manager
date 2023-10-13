60a08c74dcf87b4cde6e9d1dd866bc57da04e44a
Juan Sebastian Garcia <juansgperez@gmail.com>
2023-10-13 10:50:13 -0500

    web service configuration

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask import render_template
 
-from Controllers import ControllerCreditCard
+from Controllers import ControllerCreditCard, ControllerPaymentPlan
 from Models.CreditCard import CreditCard
 
 from datetime import date
@@ -56,8 +56,56 @@ def simulate_purchase():
         total_interest = CreditCard.calc_total_interest(search_credit_card, purchase_amount, payments)
 
         return {"status": "ok", "monthly_payment": f"{monthly_amount}", "total_interest": f"{total_interest}"}
+
     except Exception as err:
-        return {"status": "error", "mensaje": "La peticion no se puede completar", "error": str(err)}
+        return {"status": "error",
+                "message": "Request could not be completed",
+                "error": str(err)}
+
+
+@app.route('/api/simulate/saving')
+def simulate_planned_saving():
+    """
+    The function for planned saving receives a credit card and get the interest rate from that credit card
+    """
+
+    try:
+
+        card_number = request.args["card_number"]
+        purchase_amount = float(request.args["purchase_amount"])
+        payments = int(request.args["payments"])
+
+        search_credit_card = ControllerCreditCard.search_by_card_id(card_number)
+
+        monthly_amount = CreditCard.calc_monthly_payment(search_credit_card, purchase_amount, payments)
+        planned_saving = CreditCard.calc_planned_saving(search_credit_card, monthly_amount, purchase_amount)
+
+        return {"status": "ok",
+                "months": planned_saving}
+
+    except Exception as err:
+        return {"status": "error",
+                "message": "Request could not be completed",
+                "error": str(err)}
+
+
+@app.route('/api/purchase/new')
+def simulate_payment_plan():
+    try:
+        card_number = request.args["card_number"]
+        purchase_amount = float(request.args["purchase_amount"])
+        purchase_date = date.fromisoformat(request.args["purchase_date"])
+        payments = int(request.args["payments"])
+
+        ControllerPaymentPlan.insert_payment_plan(card_number, purchase_amount, purchase_date, payments)
+
+        return {"status": "ok"}
+
+    except Exception as err:
+        return {"status": "error",
+                "message": "Request could not be completed",
+                "error": str(err)}
+
 
 if __name__=='__main__':
    app.run( debug=True )
